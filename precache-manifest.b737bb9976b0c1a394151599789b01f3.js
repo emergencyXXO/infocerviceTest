9eb7e06643a4ba81b81989765ff50e6de5e063d4
jekap <eugene.pob@leverage.it>
2019-11-25 18:24:35 +0200

    Updates

---
+++ precache-manifest.b737bb9976b0c1a394151599789b01f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affdbfc0a49f58a8194d733c2529e1bc",
+    "url": "/emergencyXXO/infocerviceTest/index.html"
+  },
+  {
+    "revision": "988434ce5872e4f93cae",
+    "url": "/emergencyXXO/infocerviceTest/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e358517a9cd0f3dc1c4f",
+    "url": "/emergencyXXO/infocerviceTest/static/css/main.87c6cf64.chunk.css"
+  },
+  {
+    "revision": "988434ce5872e4f93cae",
+    "url": "/emergencyXXO/infocerviceTest/static/js/2.99051183.chunk.js"
+  },
+  {
+    "revision": "e358517a9cd0f3dc1c4f",
+    "url": "/emergencyXXO/infocerviceTest/static/js/main.6485da0a.chunk.js"
+  },
+  {
+    "revision": "eec7aae86fd858312069",
+    "url": "/emergencyXXO/infocerviceTest/static/js/runtime-main.53db0724.js"
+  }
+]);
